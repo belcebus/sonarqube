cd7ba410a91af306dfad0df8615bd72168dd60a2
David Hernández Martín <17512929+belcebus@users.noreply.github.com>
2023-04-04 08:26:25 +0000

    cambios

--- run-9.x.sh
+++ run-9.x.sh
@@ -118,9 +118,6 @@ docker run -d --name sonarqube8x \
 	         -v sonarqube8x_logs:/opt/sonarqube/logs \
            sonarqube:8.9.10-community
 
-# Set default java version to 17
-sdk default java 17.0.6-ms
-
 # CU: 09 - Run Build and SonarScanner using maven for app in java-17 using java17.
 sdk default java 17.0.6-ms && rm -rf /home/codespace/.sonar && mvn clean verify sonar:sonar \
   -Dsonar.projectKey=java-17 \
@@ -138,7 +135,7 @@ sdk default java 17.0.6-ms && rm -rf /home/codespace/.sonar && mvn clean sonar:s
   -Dsonar.login=admin -Dsonar.password=root \
   -f java-17/pom.xml
 
-# expected output: KO -> por no tener compilados los binarios
+# expected output: KO -> por no tener compilados los binarios / versión de java (17) incompatible
 # real output: [ERROR] Failed to execute goal org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.1.2184:sonar (default-cli) on project sonartest: Your project contains .java files, please provide compiled classes with sonar.java.binaries property, or exclude them from the analysis with sonar.exclusions property.
 
 # CU: 11 - Run build and SonarScanner using maven for app in java-11 using java17.
@@ -181,7 +178,7 @@ sdk default java 11.0.18-ms && rm -rf /home/codespace/.sonar && mvn clean sonar:
   -Dsonar.login=admin -Dsonar.password=root \
   -f java-17/pom.xml
 
-# expected output: KO -> No binarios compilados
+# expected output: KO -> No binarios compilados (no hacemos compilación para que no falle))
 # real output: [ERROR] Failed to execute goal org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.1.2184:sonar (default-cli) on project sonartest: Your project contains .java files, please provide compiled classes with sonar.java.binaries property, or exclude them from the analysis with sonar.exclusions property.
 
 # CU: 15 - Run build and SonarScanner using maven for java 11 using java11.
@@ -203,6 +200,69 @@ sdk default java 11.0.18-ms && rm -rf /home/codespace/.sonar && mvn clean sonar:
 
 # expected output: KO -> falta de binarios compilados
 # real output: OK
+
+# CU: 17 - Run Build and SonarScanner using scanner for app in java-17 using scanner cli Docker.
+# primero compilamos y ejecutamos el scanner:
+sdk default java 17.0.6-ms && rm -rf /home/codespace/.sonar && mvn clean verify -f java-17/pom.xml && \
+docker run \
+    --rm \
+    -e SONAR_HOST_URL="https://belcebus-obscure-trout-5gp5g4rx97527r9r-9001.preview.app.github.dev" \
+    -e SONAR_SCANNER_OPTS="-Dsonar.projectKey=java-17 -Dsonar.java.binaries=/usr/src/target/classes" \
+    -e SONAR_LOGIN="admin" \
+    -e SONAR_PASSWORD="root" \
+    -v "/workspaces/sonarqube/java-17:/usr/src" \
+    sonarsource/sonar-scanner-cli
+
+# expected output: KO -> versiones de java (17) imcompatible con sonar 8.x
+# real output: OK
+
+# CU: 18 - Run Build and SonarScanner using scanner for app in java-11 using scanner cli Docker.
+sdk default java 11.0.18-ms && rm -rf /home/codespace/.sonar && mvn clean verify -f java-11/pom.xml && \
+docker run \
+    --rm \
+    -e SONAR_HOST_URL="https://belcebus-obscure-trout-5gp5g4rx97527r9r-9001.preview.app.github.dev" \
+    -e SONAR_SCANNER_OPTS="-Dsonar.projectKey=java-11 -Dsonar.java.binaries=/usr/src/target/classes" \
+    -e SONAR_LOGIN="admin" \
+    -e SONAR_PASSWORD="root" \
+    -v "/workspaces/sonarqube/java-11:/usr/src" \
+    sonarsource/sonar-scanner-cli
+
+# expected output: OK
+# real output: OK
+
+# CU: 19 - Run Build and SonarScanner using scanner for app in java-17 using scanner cli Docker.
+# primero compilamos y ejecutamos el scanner:
+sdk default java 11.0.18-ms && rm -rf /home/codespace/.sonar && mvn clean verify -f java-17/pom.xml && \
+docker run \
+    --rm \
+    -e SONAR_HOST_URL="https://belcebus-obscure-trout-5gp5g4rx97527r9r-9001.preview.app.github.dev" \
+    -e SONAR_SCANNER_OPTS="-Dsonar.projectKey=java-17 -Dsonar.java.binaries=/usr/src/target/classes" \
+    -e SONAR_LOGIN="admin" \
+    -e SONAR_PASSWORD="root" \
+    -v "/workspaces/sonarqube/java-17:/usr/src" \
+    sonarsource/sonar-scanner-cli
+
+# expected output: KO -> java 11 no puede compilar
+# real output: [ERROR] Failed to execute goal org.apache.maven.plugins:maven-compiler-plugin:3.8.0:compile (default-compile) on project sonartest: Fatal error compiling: error: invalid target release: 17
+
+# CU: 20 - Run Build and SonarScanner using scanner for app in java-11 using scanner cli Docker.
+sdk default java 17.0.6-ms && rm -rf /home/codespace/.sonar && mvn clean verify -f java-11/pom.xml && \
+docker run \
+    --rm \
+    -e SONAR_HOST_URL="https://belcebus-obscure-trout-5gp5g4rx97527r9r-9001.preview.app.github.dev" \
+    -e SONAR_SCANNER_OPTS="-Dsonar.projectKey=java-11 -Dsonar.java.binaries=/usr/src/target/classes" \
+    -e SONAR_LOGIN="admin" \
+    -e SONAR_PASSWORD="root" \
+    -v "/workspaces/sonarqube/java-11:/usr/src" \
+    sonarsource/sonar-scanner-cli
+
+# expected output: OK
+# real output: OK
+
+
+# CU: 21 - Run Build and SonarScanner using scanner for app in java-17 using scanner cli binary.
+
+
 
 ### Scanner: Docker
 
